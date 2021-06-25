286a1d368234b09f0c00439ae51a23d566f1fbf7
Xavier Benavent <xavibenavent@gmail.com>
2021-06-25 11:10:13 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    df = pd.read_csv('docs/regulations_2019.csv')
+    print(df.head())
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
